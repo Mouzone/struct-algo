5d155b98d7314ce7e944c8ee01278b1c8ca93bf4
Mouzone <sunnyliu010@gmail.com>
2024-08-08 10:50:22 -0400

    Add logic to increase size

--- hash-map/hash_map.mjs
+++ hash-map/hash_map.mjs
@@ -1,25 +1,40 @@
-class HashMap {
+export class HashMap {
     constructor() {
         this.buckets = []
-        this.bucket_size = 100
+        this.capacity = 16
 
         this.curr_keys = []
         this.num_keys = 0
+        this.load_factor = .75
     }
 
     set(key, value) {
-        const hashed_key = hash(key, this.bucket_size)
+        const hashed_key = hash(key, this.capacity)
         if (!this.curr_keys[hashed_key] || this.curr_keys[hashed_key] === key) {
             if (!this.curr_keys[hashed_key]) {
                 this.num_keys++
             }
             this.buckets[hashed_key] = value
         }
-        // todo: figure out colliision scenario
+
+        if (this.num_keys > this.load_factor * this.capacity) {
+            const old_capacity = this.capacity
+            const new_buckets = []
+            const new_curr_keys = []
+            this.capacity *= 2
+            this.num_keys.forEach(key => {
+                const old_hashed_key = hash(key, old_capacity)
+                const new_hashed_key = hash(key, this.capacity)
+                new_curr_keys[new_hashed_key] = key
+                new_buckets[new_hashed_key] = this.buckets[old_hashed_key]
+            })
+            this.buckets = new_buckets
+            this.curr_keys = new_curr_keys
+        }
     }
 
     get(key) {
-        const hashed_key = hash(key, this.bucket_size)
+        const hashed_key = hash(key, this.capacity)
         if (this.curr_keys[hashed_key] === key) {
             return this.buckets[hashed_key]
         } else {
@@ -28,12 +43,12 @@ class HashMap {
     }
 
     has(key) {
-        const hashed_key = hash(key, this.bucket_size)
+        const hashed_key = hash(key, this.capacity)
         return this.curr_keys[hashed_key] === key
     }
 
     remove(key) {
-        const hashed_key = hash(key, this.bucket_size)
+        const hashed_key = hash(key, this.capacity)
         if (this.curr_keys[hashed_key] === key) {
             this.curr_keys[hashed_key] = null
             this.buckets[hashed_key] = null
@@ -84,12 +99,12 @@ class HashMap {
     }
 }
 
-function hash(key, bucket_size) {
+function hash(key, capacity) {
     let hashCode = 0
 
     const primeNumber = 31
     for (let i = 31; i < key.length; i++) {
-        hashCode = (primeNumber * hashCode + key.charCode.at(i)) % bucket_size
+        hashCode = (primeNumber * hashCode + key.charCode.at(i)) % capacity
     }
 
     return hashCode
