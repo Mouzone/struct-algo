22688df4d736cd73dd5d4031ae039efa99cc17bd
Mouzone <sunnyliu010@gmail.com>
2024-08-09 12:04:21 -0400

    Write delete and delete helper and fix insert

--- binary-search-trees/binary_search_tree.mjs
+++ binary-search-trees/binary_search_tree.mjs
@@ -9,8 +9,6 @@ export class Tree {
     buildTree(array) {
         if (array.length === 0) {
             return null
-        } else if (array.length === 1) {
-            return new Node(array[0])
         }
 
         const middle = Math.floor(array.length / 2)
@@ -20,6 +18,97 @@ export class Tree {
         root.right = this.buildTree(array.slice(middle+1))
 
         return root
+    }
+
+    insert(value) {
+        // assume this.root will always have at least one node
+        // assume value is not duplicate of value already in
+        const new_node = new Node(value)
+        let iterator = this.root
+        while (iterator) {
+            if (value > iterator.value) {
+                if (!iterator.right) {
+                    iterator.right = new_node
+                    return
+                }
+                iterator = iterator.right
+            } else {
+                if (!iterator.left) {
+                    iterator.left = new_node
+                    return
+                }
+                iterator = iterator.left
+            }
+        }
+    }
+
+    delete(value) {
+        let iterator = this.root
+        while (iterator) {
+            if (iterator.value === value) {
+                this.root = this.delete_helper(iterator)
+                return true
+            } else if (iterator.left && iterator.left.value === value) {
+                iterator.left = this.delete_helper(iterator.left)
+                return true
+            } else if (iterator.right && iterator.right.value === value) {
+                iterator.right = this.delete_helper(iterator.right)
+                return true
+            }
+
+            if (value > iterator.value) {
+
+                iterator = iterator.right
+
+            } else if (value < iterator.value) {
+
+                iterator = iterator.left
+
+            }
+        }
+        return false
+    }
+
+    delete_helper(node_to_remove) {
+        if (!node_to_remove.left && !node_to_remove.right) {
+
+            return null
+
+        } else if (!node_to_remove.left) {
+
+            return node_to_remove.right
+
+        } else if (!node_to_remove.right) {
+
+            return node_to_remove.left
+
+        } else {
+            // node_to_remove has both children
+            let iterator = node_to_remove.right
+            let prev = node_to_remove
+            while (iterator) {
+                if (!iterator.left) {
+                    // arrived at the replacement node
+                    // set its child_node as its replacement
+                    // if prev is node_to_remove then its right become iterator's right
+                    // --> which then iterator.right becomes its old right branch
+                    if (node_to_remove === prev) {
+                        node_to_remove.right = iterator.right
+                    } else {
+                        prev.left = iterator.right
+                    }
+
+                    iterator.left = node_to_remove.left
+                    iterator.right = node_to_remove.right
+                    
+                    break
+                }
+                prev = iterator
+                iterator = iterator.left
+            }
+
+            return iterator
+        }
     }
 }
 
--- binary-search-trees/main.mjs
+++ binary-search-trees/main.mjs
@@ -1,16 +1,35 @@
 import {Tree} from "./binary_search_tree.mjs";
 
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node === null) {
-        return;
+        return
     }
     if (node.right !== null) {
-        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false)
     }
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
     if (node.left !== null) {
-        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true)
     }
-};
+}
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+prettyPrint(tree.root)
+// tree.insert(10)
+// prettyPrint(tree.root)
+// tree.insert(0)
+// prettyPrint(tree.root)
+
+// tree.delete(0)
+// prettyPrint(tree.root)
+// tree.delete(9)
+// prettyPrint(tree.root)
+tree.delete(4)
+prettyPrint(tree.root)
+tree.delete(8)
+prettyPrint(tree.root)
+tree.delete(67)
+tree.delete(6345)
+prettyPrint(tree.root)
+tree.delete(9)
 prettyPrint(tree.root)
